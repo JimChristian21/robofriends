9756cd5d6f7a5d6f1b5da4990371ad99a6500f13
JimChristian21 <jimchristianedullantes9@gmail.com>
2023-08-17 23:48:00 +0800

    Added functionality of SearchBox Component and dynamic CardList

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import CardList from "./CardList";
 import { robots } from "./robot";
 import SearchBox from "./SearchBox";
 
 const App = () => {
 
+    const [robotList, setRobotList] = useState([]);
+
+    useEffect(() => {
+
+        setRobotList(robots);
+    }, []);
+
+    const filterRobot = (name) => {
+
+        let fileteredRobot = robots.filter((item) => item.name.includes(name));
+        setRobotList(fileteredRobot);
+    }
+
     return (
         <div className="tc">
             <h1>RoboFriends</h1>
-            <SearchBox/>
-            <CardList list={robots}/>
+            <SearchBox filterRobot={filterRobot}/>
+            <CardList list={robotList}/>
         </div>
         
     );
--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const SearchBox = () => {
+const SearchBox = ({ filterRobot }) => {
 
     return (
         <div className="pa2">
@@ -8,6 +8,7 @@ const SearchBox = () => {
                 className="pa3 ba b--green bg-lightest-blue"
                 type="search" 
                 placeholder="search robots"
+                onChange={(e) => filterRobot(e.target.value)}
             />
         </div>
        
