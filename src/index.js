7bd5e40a6425554ab86ebddc338393c4615cc71d
JimChristian21 <jimchristianedullantes9@gmail.com>
2023-08-17 23:33:23 +0800

    Added SearchBox component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import CardList from './CardList';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
 import { robots } from './robot';
@@ -8,7 +8,7 @@ import { robots } from './robot';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <CardList list={robots}/>
+    <App />
   </React.StrictMode> 
 );
 
